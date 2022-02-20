f28bf3684eda58636deaa9bca5f6051f5681ba50
Omar Villafuerte <ovillafuerte.94@gmail.com>
2022-02-20 00:53:54 -0500

    Create Provider

---
+++ src/Provider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace IngeniusHq\HelloLaravel;
+
+use Illuminate\Support\ServiceProvider;
+
+class Provider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('hello', function () {
+            return new Hello();
+        });
+    }
+}
