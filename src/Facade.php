57c5bf41221785309b11b6f9f5af15fd67178463
Omar Villafuerte <ovillafuerte.94@gmail.com>
2022-02-20 00:54:00 -0500

    Create Facade

---
+++ src/Facade.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace IngeniusHq\HelloLaravel;
+
+use Illuminate\Support\Facades\Facade as BaseFacade;
+
+class Facade extends BaseFacade
+{
+    /**
+     * Get the registered name of the component.
+     */
+    public static function getFacadeAccessor()
+    {
+        return 'hello';
+    }
+}
